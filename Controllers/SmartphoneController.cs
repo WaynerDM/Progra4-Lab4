d744b4f38b3307e2131778c7ebfda2aa8ebd3a01
Wayner Mora <morawayner3@gmail.com>
2026-05-07 19:43:15 -0600

    Nuveo controlador acerca de smartphones.

---
+++ Controllers/SmartphoneController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SmartphoneController : ControllerBase
+    {
+        private static readonly string[] Marcas = new[]
+        {
+            "Samsung", "Apple", "Xiaomi", "Motorola", "Huawei"
+        };
+
+        private readonly ILogger<SmartphoneController> _logger;
+
+        public SmartphoneController(ILogger<SmartphoneController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetSmartphones")]
+        public IEnumerable<Smartphone> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Smartphone
+            {
+                Id = index,
+                Marca = Marcas[Random.Shared.Next(Marcas.Length)],
+                Precio = Random.Shared.Next(300, 2000),
+                Modelo = $"Modelo {index}"
+            })
+            .ToArray();
+        }
+    }
+
+    public class Smartphone
+    {
+        public int Id { get; set; }
+
+        public string? Marca { get; set; }
+
+        public int Precio { get; set; }
+
+        public string? Modelo { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,11 +10,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.gfgdgdf
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
